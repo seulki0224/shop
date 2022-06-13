7372e736eb0e3f9a3379aba9a8eda30243a0af06
seulki0224 <sur1988@naver.com>
2022-06-13 22:40:27 +0900

    Lifecycle과 useEffect 2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,45 @@
 // import { type } from "@testing-library/user-event/dist/type";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 // import styled from "styled-components";
 
 function Detail(props) {
+
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(true);
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(x => x.id == id);
+
+  //2초뒤에 div 사라짐
+  /* useEffect(() => {
+    setTimeout(()=>{ setAlert(false) }, 2000)
+  }) */
+
+  //Dependency [] <- useEffect 실행조건을 넣을수 있는곳
+  /* useEffect(() => {    
+    //setTimeout(()=>{ setAlert(false) }, 2000)
+    setTimeout(()=>{ setAlert(false) }, 2000)
+    console.log(1)
+  }
+  //, []);    //컴포넌트 mount시 1회만 실행
+  , [count])  //[count]<- 카운트 변수가 변할때 실행 됨
+  */
+
+  //return() => {} useEffect 동작 전에 실행되는 리턴코드
+  /* useEffect(() => {
+    
+    let a = setTimeout(()=>{ setAlert(false) }, 2000)    
+    console.log("useEffect실행전")
+
+    return () => {      
+      clearTimeout(a)
+      console.log("기존타이머 제거")
+      //기존 데이터 영역 제거 해주세요 등으로 활용
+    }
+
+  }) */
+
+  //숫자만 입력 가능
   let [num, setNum] = useState("");
 
   useEffect(() => {
@@ -12,42 +47,28 @@ function Detail(props) {
       console.log("그러지마세요");
     }
   }, [num]);
-
-  /* setTimeout(() => {
-      setAlert(false);
-    }, 2000);
-   */
-  /*  let a = setTimeout(() => {
-      setAlert(false);
-    }, 2000);
-    console.log("222");
-    return () => {
-      console.log("useEffect실행전 ");
-      clearTimeout(a);
-      console.log("기존타이머 제거");
-    };
-  }, []); //컴포넌트 mount시 1회만 실행
-  //}, [count]); //count값이 변할때 */
-
-  let [count, setCount] = useState(0);
-  let [alert, setAlert] = useState(true);
-  let { id } = useParams();
-  let 찾은상품 = props.shoes.find(x => x.id == id);
-
+  
   return (
     <div className="container">
-      <input
-        onChange={e => {
+      
+      {/* alert, setAlert*/}
+      { 
+        alert == true 
+        ? <div className="alertaaa alert alert-warning"> 2초이내 구매시 할인 </div>
+        : null
+      }      
+      <br/><br/>
+
+      {/* [num] */}
+      <input onChange={ e => {
           setNum(e.target.value);
         }}
       />
+      <br/><br/>
 
-      {alert == true ? (
-        <div className="alertaaa alert alert-warning">2초이내 구매시 할인</div>
-      ) : null}
-      {count}
-
+      {count} 
       <button onClick={() => setCount(count + 1)}>버튼</button>
+      
       <div className="row">
         <div className="col-md-6">
           <img src={`../img/shoes${id}.jpg`} width="100%" />
