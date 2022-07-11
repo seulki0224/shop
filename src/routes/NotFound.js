fdc8500f091be9cd6ca4307f95ab5a59541f0dcc
seulki0224 <42800798+seulki0224@users.noreply.github.com>
2022-07-12 00:52:53 +0900

    Update NotFound.js

--- src/routes/NotFound.js
+++ src/routes/NotFound.js
@@ -5,7 +5,7 @@ function NotFound(){
         
         <div className="container">
             <div className="row" style={{margin:"30px 0"}}>
-                <img src='./img/404.jpg'/>
+                <img src='https://img.freepik.com/premium-vector/404-error-rocket-launching-to-space-modern-background-design_516327-117.jpg?w=1480'/>
             </div>
         </div> 
 
